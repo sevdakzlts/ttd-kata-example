1188d82369c2dc5404b1b1a24da23183c150901f
sevdakzlts <sevdakzlts.76@gmail.com>
2022-06-23 13:02:33 +0300

    REFACTOR Create checkSums for check all sums

--- string-calculator_test.go
+++ string-calculator_test.go
@@ -1,12 +1,30 @@
 package calculator
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
-	sum := Add("2,2")
-	expected := 4
 
-	if sum != expected {
-		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	checkSums := func(t *testing.T, got, want int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
 	}
+
+	t.Run("make the sums of input", func(t *testing.T) {
+		got := Add("2,2")
+		want := 4
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safetly sum empty string", func(t *testing.T) {
+		got := Add("")
+		want := 0
+
+		checkSums(t, got, want)
+	})
+
 }
