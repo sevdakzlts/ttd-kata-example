7f882e7f9f075605f84926027bd707ae6815c76e
sevdakzlts <sevdakzlts.76@gmail.com>
2022-06-24 14:30:55 +0300

    GREEN

--- string-calculator.go
+++ string-calculator.go
@@ -12,10 +12,16 @@ func Add(input string) (int, error) {
 	matchDelimeters := delimiters.FindAllString(input, -1)
 
 	lastTwoCharacter := strings.LastIndex(input, "\n")
+	negativeNumbers := regexp.MustCompile("[-]")
+	matchNegatives := negativeNumbers.FindAllString(input, -1)
 	if lastTwoCharacter == len(input)-1 {
 		err := errors.New("the input is not valid")
 		return 0, err
 
+	}
+	if len(matchNegatives) != 0 {
+		err := errors.New("negatives not allowed")
+		return 0, err
 	} else {
 		var sum int
 		for _, i := range matchDelimeters {
