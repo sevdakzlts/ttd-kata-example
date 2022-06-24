c8fe1072cb3abdadae3c550349e85805f05bcddf
sevdakzlts <sevdakzlts.76@gmail.com>
2022-06-24 14:18:52 +0300

    GREEN

--- string-calculator.go
+++ string-calculator.go
@@ -2,22 +2,23 @@ package calculator
 
 import (
 	"errors"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
 func Add(input string) (int, error) {
+	delimiters := regexp.MustCompile("[0-9]")
+	matchDelimeters := delimiters.FindAllString(input, -1)
+
 	lastTwoCharacter := strings.LastIndex(input, "\n")
 	if lastTwoCharacter == len(input)-1 {
 		err := errors.New("the input is not valid")
 		return 0, err
 
 	} else {
-
-		inputWithoutNewLine := strings.Replace(input, "\n", ",", -1)
-		inputChars := strings.Split(inputWithoutNewLine, ",")
 		var sum int
-		for _, i := range inputChars {
+		for _, i := range matchDelimeters {
 			number, _ := strconv.Atoi(i)
 			sum += number
 		}
